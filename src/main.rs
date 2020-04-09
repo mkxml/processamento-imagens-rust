9b60bb7e87303912c088501810623889117069c6
Matheus Kautzmann <mkvchain@gmail.com>
2020-04-09 20:10:05 -0300

    Add new demos

--- src/main.rs
+++ src/main.rs
@@ -8,56 +8,51 @@ mod load;
 // Importando tipo PixelMatrix
 use img::PixelMatrix;
 
-// Fixando o tamanho do canvas
-// TODO: Para futuros exercícios, tornar o canvas dinâmico, atualmente fixo em 2000x2000
-const CANVAS_WIDTH: u32 = 2000;
-const CANVAS_HEIGHT: u32 = 2000;
+// Tamanho default do canvas
+const DEFAULT_CANVAS_WIDTH: u32 = 2000;
+const DEFAULT_CANVAS_HEIGHT: u32 = 2000;
 
 fn main() {
     // Carregando imagens de origem
-    let equilateral = load::open_image("equilatero.jpg");
-    let isoceles = load::open_image("isoceles.jpg");
-    let scalene = load::open_image("escaleno.jpg");
-    let obtuse = load::open_image("obtusangulo.jpg");
-    let right_triangle = load::open_image("retangulo.jpg");
-
-    // Cria o primeiro canvas para armanzenar o resultado das transformações
-    let mut canvas = img::new_canvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+    let mut demo1 = load::open_image("exercicio1.jpg");
+    let demo2 = load::open_image("exercicio2.jpg");
+    let mut demo3 = load::open_image("exercicio3.jpg");
+    let mut demo4 = load::open_image("exercicio4.jpg");
+    let mut demo5 = load::open_image("exercicio5.jpg");
 
     /* Aplicando transformações */
 
-    // Amplia o equilatero em 2x, salva e zera o canvas
-    img::scale(&equilateral, &mut canvas, 2.0, 2.0);
-    save_as_image("Equilatero", &canvas, "equilatero_transformado.jpg");
-    canvas = img::new_canvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+    // Exercicio 1
+    img::brightness(&mut demo1, 30);
+    img::contrast(&mut demo1, 1.4);
+    img::median_filter(&mut demo1);
+    save("Exercicio 1", &demo1, "exercicio1_output.jpg");
 
-    // Diminui o isoceles pela metade
-    img::scale(&isoceles, &mut canvas, 0.5, 0.5);
-    save_as_image("Isóceles", &canvas, "isoceles_transformado.jpg");
-    canvas = img::new_canvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+    // Exercicio 2
+    let (width, height) = demo2.dimensions();
+    let mut canvas = img::new_canvas(width, height);
+    img::border_detection(&demo2, &mut canvas, 10);
+    save_as_image("Exercicio 2", &canvas, "exercicio2_output.jpg");
 
-    // Rotaciona o escaleno em 90 graus no sentido anti-horário
-    img::rotate(&scalene, &mut canvas, -90.0);
-    save_as_image("Escaleno", &canvas, "escaleno_transformado.jpg");
-    canvas = img::new_canvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+    // Exercicio 3
+    img::brightness(&mut demo3, 50);
+    img::contrast(&mut demo3, 1.25);
+    save("Exercicio 3", &demo3, "exercicio3_output.jpg");
 
-    // Transladando o retângulo em 200 pixels em cada eixo
-    img::translate(&right_triangle, &mut canvas, 200, 200);
-    save_as_image("Retângulo", &canvas, "retangulo_transformado.jpg");
-    canvas = img::new_canvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+    // Exercicio 4
+    img::gaussian_filter(&mut demo4);
+    save("Exercicio 4", &demo4, "exercicio4_output.jpg");
 
-    // Espelhando o obtusângulo na vertical
-    img::flip(&obtuse, &mut canvas, img::Direction::Vertical);
-    save_as_image("Obtusângulo", &canvas, "obtusangulo_transformado.jpg");
-    canvas = img::new_canvas(CANVAS_WIDTH, CANVAS_HEIGHT);
-
-    // Espelhando o triângulo retângulo na vertical
-    img::flip(&right_triangle, &mut canvas, img::Direction::Horizontal);
-    save_as_image(
-        "Retângulo espelhado",
-        &canvas,
-        "retangulo_espelhado_transformado.jpg",
+    // Exercicio 5
+    let (width, height) = demo5.dimensions();
+    canvas = img::new_canvas(width, height);
+    img::average_filter(&mut demo5, &mut canvas);
+    img::threshold(
+        &mut img::pixel_matrix_to_image(&canvas, width, height),
+        &mut canvas,
+        60,
     );
+    save_as_image("Exercicio 5", &canvas, "exercicio5_output.jpg");
 }
 
 fn save_as_image(name: &str, canvas: &PixelMatrix, filename: &str) {
@@ -65,6 +60,13 @@ fn save_as_image(name: &str, canvas: &PixelMatrix, filename: &str) {
     let height = canvas[0].len();
     let output_image = img::pixel_matrix_to_image(canvas, width as u32, height as u32);
     match output_image.save(filename) {
+        Ok(()) => println!("{:?} processado, salvo como {:?}", name, filename),
+        Err(err) => println!("Erro ao salvar a imagem: {:?}", err),
+    }
+}
+
+fn save(name: &str, img: &image::RgbImage, filename: &str) {
+    match img.save(filename) {
         Ok(()) => println!("{:?} processado, salvo como {:?}", name, filename),
         Err(err) => println!("Erro ao salvar a imagem: {:?}", err),
     }
